99938ed3ab976335191affd098afb9809c13611e
hugobillemartins@gmail.com <hugobillemartins@gmail.com>
2024-12-29 18:44:15 -0300

    comments, new version and datta structure

--- pathfinding.c
+++ pathfinding.c
@@ -4,127 +4,184 @@
 #include <string.h>
 #include <stdlib.h>
 #include <time.h>
-#include <limits.h>  // Para definir o valor de INF
+#include <limits.h>
 #include <math.h>
 
-
-int heuristic(int node_y, int node_x, int end_y, int end_x){
-
-    return abs(node_x - end_x) + abs(node_y - end_y);
-}
-///////////////////////////// ALGORITMO A* /////////////////////////
-
-typedef struct PriorityQueue {
-    int* nodes;            
-    int* priorities;       
-    int size;
-    int capacity;
-} PriorityQueue;
-
-PriorityQueue* createPriorityQueue(int capacity) {
-    PriorityQueue* pq = (PriorityQueue*)malloc(sizeof(PriorityQueue));
-    pq->nodes = (int*)malloc(capacity * sizeof(int));
-    pq->priorities = (int*)malloc(capacity * sizeof(int));
-    pq->size = 0;
-    pq->capacity = capacity;
-    return pq;
+int heuristic(int node, int goal)
+{
+    return abs(node - goal);
 }
 
-void push(PriorityQueue* pq, int node, int priority) {
-    if (pq->size == pq->capacity) return;
-    pq->nodes[pq->size] = node;
-    pq->priorities[pq->size] = priority;
-    pq->size++;
+int *createVetorInt(int size)
+{
+    return (int *)malloc(size * sizeof(int));
 }
 
-int pop(PriorityQueue* pq) {
-    int minIndex = 0;
-    for (int i = 1; i < pq->size; i++) {
-        if (pq->priorities[i] < pq->priorities[minIndex]) {
-            minIndex = i;
+void addNoh(Noh **caminho, int valor)
+{
+    Noh *novoNoh = (Noh *)malloc(sizeof(Noh));
+    novoNoh->valor = valor;
+    novoNoh->proximo = NULL;
+
+    if (*caminho == NULL)
+    {
+        *caminho = novoNoh;
+    }
+    else
+    {
+        Noh *temp = *caminho;
+        while (temp->proximo != NULL)
+        {
+            temp = temp->proximo;
         }
+        temp->proximo = novoNoh;
     }
-    int minNode = pq->nodes[minIndex];
-    pq->size--;
-    for (int i = minIndex; i < pq->size; i++) {
-        pq->nodes[i] = pq->nodes[i + 1];
-        pq->priorities[i] = pq->priorities[i + 1];
-    }
-    return minNode;
 }
 
-int contains(PriorityQueue* pq, int node) {
-    for (int i = 0; i < pq->size; i++) {
-        if (pq->nodes[i] == node) return 1;
+// usado para a lista aberta e fechada
+typedef struct
+{
+    int *nodes;
+    int size;
+} NodeList;
+
+NodeList createNodeList(int capacity)
+{
+    NodeList list;
+    list.nodes = (int *)malloc(capacity * sizeof(int));
+    list.size = 0;
+    return list;
+}
+
+void addNodeToList(NodeList *list, int node)
+{
+    list->nodes[list->size++] = node;
+}
+
+void removeNodeFromList(NodeList *list, int index)
+{
+    for (int i = index; ehMenor(i, list->size - 1); i++)
+    {
+        list->nodes[i] = list->nodes[i + 1];
+    }
+    list->size--;
+}
+
+int isNodeInList(NodeList *list, int node)
+{
+    for (int i = 0; ehMenor(i, list->size); i++)
+    {
+        if (ehIgual(list->nodes[i], node))
+        {
+            return 1;
+        }
     }
     return 0;
 }
 
-void addNodeToPath(Noh** caminho, int valor) {
-    Noh* novoNoh = (Noh*)malloc(sizeof(Noh));
-    novoNoh->valor = valor;
-    novoNoh->proximo = *caminho;
-    *caminho = novoNoh;
+void freeNodeList(NodeList *list)
+{
+    free(list->nodes);
+    list->size = 0;
 }
 
-int findPath(int** adjMatrix, int numNos, int inicio, int fim, Noh** caminho, int* visitados) {
-    int* gScores = (int*)malloc(numNos * sizeof(int));
-    int* fScores = (int*)malloc(numNos * sizeof(int));
-    int* cameFrom = (int*)malloc(numNos * sizeof(int));
-    
-    for (int i = 0; i < numNos; i++) {
-        gScores[i] = INT_MAX;
-        fScores[i] = INT_MAX;
-        cameFrom[i] = -1;
-        visitados[i] = 0;
+int findPath(int **adjMatrix, int numNodes, int startNode, int endNode, Noh **path, int *visitedNodes)
+{
+    int *costFromStart = createVetorInt(numNodes);
+    int *heuristicCost = createVetorInt(numNodes);
+    int *totalCost = createVetorInt(numNodes);
+    int *previousNode = createVetorInt(numNodes);
+
+    NodeList openList = createNodeList(numNodes);
+    NodeList closedList = createNodeList(numNodes);
+
+    int visitCounter = 0;
+
+    // Initialize vectors
+    for (int i = 0; ehMenor(i, numNodes); i++)
+    {
+        costFromStart[i] = INT_MAX;
+        heuristicCost[i] = 0;
+        totalCost[i] = INT_MAX;
+        previousNode[i] = -1;
+        visitedNodes[i] = 0;
     }
 
-    gScores[inicio] = 0;
-    fScores[inicio] = heuristic(0, inicio, 0, fim); // Update with correct node coordinates if needed
+    // Initialize start node
+    costFromStart[startNode] = 0;
+    heuristicCost[startNode] = heuristic(startNode, endNode);
+    totalCost[startNode] = heuristicCost[startNode];
+    addNodeToList(&openList, startNode);
+    visitedNodes[startNode] = ++visitCounter;
 
-    PriorityQueue* openSet = createPriorityQueue(numNos);
-    push(openSet, inicio, fScores[inicio]);
+    while (openList.size > 0)
+    {
 
-    while (openSet->size > 0) {
-        int current = pop(openSet);
-        visitados[current] = 1;
-
-        if (current == fim) {
-            *caminho = NULL;
-            while (current != -1) {
-                addNodeToPath(caminho, current);
-                current = cameFrom[current];
+        // find the node with the smallest totalCost in openList
+        int currentNodeIndex = 0;
+        int currentNode = openList.nodes[0];
+        for (int i = 1; ehMenor(i, openList.size); i++)
+        {
+            if (ehMenor(totalCost[openList.nodes[i]], totalCost[currentNode]))
+            {
+                currentNode = openList.nodes[i];
+                currentNodeIndex = i;
             }
-            free(gScores);
-            free(fScores);
-            free(cameFrom);
-            free(openSet->nodes);
-            free(openSet->priorities);
-            free(openSet);
-            return 1; // Path found
         }
 
-        for (int neighbor = 0; neighbor < numNos; neighbor++) {
-            if (adjMatrix[current][neighbor] > 0 && !visitados[neighbor]) {
-                int tentative_gScore = gScores[current] + adjMatrix[current][neighbor];
-                if (tentative_gScore < gScores[neighbor]) {
-                    cameFrom[neighbor] = current;
-                    gScores[neighbor] = tentative_gScore;
-                    fScores[neighbor] = gScores[neighbor] + heuristic(0, neighbor, 0, fim); // Update coordinates if needed
+        // Remove da lista aberta e passa para a lista fechada
+        removeNodeFromList(&openList, currentNodeIndex);
+        addNodeToList(&closedList, currentNode);
 
-                    if (!contains(openSet, neighbor)) {
-                        push(openSet, neighbor, fScores[neighbor]);
-                    }
-                }
+        // End!
+        if (currentNode == endNode)
+        {
+            // build path
+            for (int node = endNode; ehDiferente(node,-1); node = previousNode[node])
+            {
+                addNoh(path, node);
+            }
+
+            free(costFromStart);
+            free(heuristicCost);
+            free(totalCost);
+            free(previousNode);
+            freeNodeList(&openList);
+            freeNodeList(&closedList);
+            return 1;
+        }
+
+        // Explore neighbors
+        for (int neighbor = 0; ehMenor(neighbor, numNodes); neighbor++)
+        {
+            // check nó válido
+            if (ehIgual(adjMatrix[currentNode][neighbor], 0) || isNodeInList(&closedList, neighbor))
+                continue;
+
+            int currentNodeCost = costFromStart[currentNode] + adjMatrix[currentNode][neighbor];
+
+            if (ehIgual(isNodeInList(&openList, neighbor), 0))
+            {
+                addNodeToList(&openList, neighbor);
+                heuristicCost[neighbor] = heuristic(neighbor, endNode);
+            }
+
+            if (ehMenor(currentNodeCost, costFromStart[neighbor]))
+            {
+                costFromStart[neighbor] = currentNodeCost;
+                totalCost[neighbor] = costFromStart[neighbor] + heuristicCost[neighbor];
+                previousNode[neighbor] = currentNode;
+                visitedNodes[neighbor] = ++visitCounter;
             }
         }
     }
 
-    free(gScores);
-    free(fScores);
-    free(cameFrom);
-    free(openSet->nodes);
-    free(openSet->priorities);
-    free(openSet);
-    return 0; // Path not found
+    // Default return, path not found
+    free(costFromStart);
+    free(heuristicCost);
+    free(totalCost);
+    free(previousNode);
+    freeNodeList(&openList);
+    freeNodeList(&closedList);
+    return 0;
 }
